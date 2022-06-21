80019c9c85a17997295f4cc507e97183b0e1e109
netro <hewi@git.com>
2022-06-21 17:36:52 +0800

    Update docker conf

--- srv/project/project/settings/production.py
+++ srv/project/project/settings/production.py
@@ -12,6 +12,21 @@ DATABASES = {
     },
 }
 
+# settings.py
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'DEBUG',
+    },
+}
+
 try:
     from .local import *
 except ImportError:
