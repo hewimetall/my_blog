03afdcaf3fb0455fe7e36c27ecb9c67a29968f81
netro <hewi@git.com>
2022-06-21 19:27:07 +0800

    fix: локального запуска без докера.

--- srv/project/home/models.py
+++ srv/project/home/models.py
@@ -38,6 +38,35 @@ class HomePage(Page):
         context["posts"] = posts
         return context
 
+
+class CategoryPage(Page):
+    template = 'page/home.html'
+
+    def get_context(self, request, *args, **kwargs):
+        """Adding custom stuff to our context."""
+        context = super().get_context(request, *args, **kwargs)
+        # Get all posts
+        all_posts = BlogPage.objects.live().public().order_by('-first_published_at')
+        # Paginate all posts by 2 per page
+        paginator = Paginator(all_posts, 10)
+        # Try to get the ?page=x value
+        page = request.GET.get("page")
+        try:
+            # If the page exists and the ?page=x is an int
+            posts = paginator.page(page)
+        except PageNotAnInteger:
+            # If the ?page=x is not an int; show the first page
+            posts = paginator.page(1)
+        except EmptyPage:
+            # If the ?page=x is out of range (too high most likely)
+            # Then return the last page
+            posts = paginator.page(paginator.num_pages)
+
+        # "posts" will have child pages; you'll need to use .specific in the template
+        # in order to access child properties, such as youtube_video_id and subtitle
+        context["posts"] = posts
+        return context
+
 class BlogPageModel(Page):
     intro = models.CharField(max_length=250, null=True, blank=True)
     body = StreamField(BodyBlock(), blank=True)
